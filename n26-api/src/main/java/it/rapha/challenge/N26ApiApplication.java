51e89c86dd458dee19a95dbf2ace30390fd1485a
Raphael Almeida <rapha.it@gmail.com>
2017-04-14 18:34:49 -0300

    Project setup with spring-boot, jetty, lombok and java 8.

---
+++ n26-api/src/main/java/it/rapha/challenge/N26ApiApplication.java
@@ -0,0 +1,12 @@
+package it.rapha.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N26ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N26ApiApplication.class, args);
+	}
+}
