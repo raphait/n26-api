8496614040bc9c99bfae50d12459907f3627f426
Raphael Almeida <rapha.it@gmail.com>
2017-04-14 18:47:00 -0300

    Adding a route / to message Hello world.

--- n26-api/src/main/java/it/rapha/challenge/N26ApiApplication.java
+++ n26-api/src/main/java/it/rapha/challenge/N26ApiApplication.java
@@ -2,9 +2,17 @@ package it.rapha.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class N26ApiApplication {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(N26ApiApplication.class, args);
